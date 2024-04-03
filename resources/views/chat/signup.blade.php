4a7e55ef8cea04e53b6b4f1537249b2287797c9f
Kanon Nakajima <kanon2155103@gmail.com>
2024-04-03 18:21:12 +0900

    いただいたアドバイスの反映

--- resources/views/chat/signup.blade.php
+++ resources/views/chat/signup.blade.php
@@ -1,17 +1,19 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('chat.signup.create') }}">
+    <form method="POST" action="{{ route('signup.create') }}">
         @csrf
         <!-- Nickname -->
         <div>
             <x-input-label for="nickname" value="ニックネーム" />
-            <x-text-input id="nickname" class="block mt-1 w-full" type="text" name="nickname" value="{{ old('nickname') }}" required autofocus autocomplete="nickname" />
+            <x-text-input id="nickname" class="block mt-1 w-full" type="text" name="nickname"
+                value="{{ old('nickname') }}" required autofocus autocomplete="nickname" />
             <x-input-error :messages="$errors->get('nickname')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" value="メールアドレス" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ old('email') }}" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                value="{{ old('email') }}" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -19,10 +21,8 @@
         <div class="mt-4">
             <x-input-label for="password" value="パスワード" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -31,15 +31,15 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" value="パスワードの再入力" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('chat.login') }}">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href="{{ route('login.create') }}">
                 すでに登録済みの方
             </a>
 
